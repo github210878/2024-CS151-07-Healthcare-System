1ce4b9de171d92090f2db9442e85f088e3ab64fb
rogueblade18 <53056908+rogueblade18@users.noreply.github.com>
2024-10-10 15:05:03 -0700

    Update Staff class

--- src/healthCare/Staff.java
+++ src/healthCare/Staff.java
@@ -1,5 +1,31 @@
 package healthCare;
+/** Staff of the healthcare system
+ * 
+ * @author Edmond Li
+ * @author
+ */
+public class Staff {
+    protected String name;
+    protected int age;
+    protected double salary;
 
-public abstract class Staff {
+    /**
+     * 
+     * @param name  the name of the staff
+     * @param age   the age of the staff
+     * @param salary    the amount of money earned
+     */
+    public Staff(String name, int age, double salary){
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
 
+    /** Setter for salary
+     * 
+     * @param salary the new salary amount
+     */
+    public setSalary(double salary){
+        this.salary = salary;
+    }
 }
